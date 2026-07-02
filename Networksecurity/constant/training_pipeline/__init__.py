174926b38023fdf118689dd2b88adab76ff1097e
satyamsrj <sarojsatyam150@gmail.com>
2026-07-02 15:32:42 +0530

    ETL pipeline is completed

---
+++ Networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+"""
+Data Ingestion related constants are defined in this file. 
+This file is created to avoid hardcoding of values.
+"""
+DATA_INGESTION_COLLECTION_NAME = "phishing"
+DATA_INGESTION_DATABASE_NAME = "Networksecurity"
+DATA_INGESTION_DIR_NAME = os.path.join("artifacts", "data_ingestion")
+DATA_INGESTION_FEATURE_STORE_DIR = os.path.join(DATA_INGESTION_DIR_NAME, "feature_store")
+DATA_INGESTION_INGESTED_DIR = os.path.join(DATA_INGESTION_DIR_NAME, "ingested")
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Defining common constants for the project. 
+This file is created to avoid hardcoding of values.
+"""
+TARGET_COLUMN = "status"
+PIPELINE_NAME: str = "Networksecurity"
+ARTIFACT_DIR: str = "artifacts"
+FILE_NAME: str = "phishing.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
---
+++ Networksecurity/entity/config_entity.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import os
+from Networksecurity.constant import training_pipeline
+
+print(training_pipeline.PIPELINE_NAME)
+print(training_pipeline.ARTIFACT_DIR)
+
+class TrainingPipelineConfig:
+    def __init__(self,time_stamp: str = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")):
+        self.pipeline_name = training_pipeline.PIPELINE_NAME
+        self.artifact_name = training_pipeline.ARTIFACT_DIR
+        self.artifact_dir = os.path.join(self.artifact_name,time_stamp)
+        self.time_stamp = time_stamp
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config: TrainingPipelineConfig):
+        self.data_ingestion_collection_name = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+        self.data_ingestion_database_name = training_pipeline.DATA_INGESTION_DATABASE_NAME
+        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME)
+        self.feature_store_dir = os.path.join(self.data_ingestion_dir,training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR)
+        self.ingested_dir = os.path.join(self.data_ingestion_dir,training_pipeline.DATA_INGESTION_INGESTED_DIR)
+        self.train_file_path = os.path.join(self.ingested_dir,training_pipeline.TRAIN_FILE_NAME)
+        self.test_file_path = os.path.join(self.ingested_dir,training_pipeline.TEST_FILE_NAME)
+        self.train_test_split_ratio = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
