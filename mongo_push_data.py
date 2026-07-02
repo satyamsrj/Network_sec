5bb1931dd81e3f59ba40162e00ec7fab0f7ba6a9
satyamsrj <sarojsatyam150@gmail.com>
2026-07-02 15:07:57 +0530

    Data added to Mongo

--- mongo_push_data.py
+++ mongo_push_data.py
@@ -1,21 +1,61 @@
-from pymongo import MongoClient
-from mongoDB_cred import MONGO_URL
+import os 
+import sys
+import json 
 
-# Connect to your existing cluster
-client = MongoClient(MONGO_URL)
+from dotenv import load_dotenv
+load_dotenv()
 
-# Choose a database under the same cluster
-db = client["NetworkSecurity_db"]
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+print(f"MONGO_DB_URI: {MONGO_DB_URI}")
 
-# Choose a collection
-collection = db["exceptions"]
+import certifi
+ca = certifi.where()
 
-# Insert sample data
-collection.insert_one({
-    "file": "exception.py",
-    "line": 21,
-    "error": "division by zero",
-    "timestamp": "2026-07-01T21:09:00"
-})
+import pandas as pd
+import pymongo
+import numpy as np
+from Networksecurity.exception.exception import NetworkSecurityException    
+from Networksecurity.logging.logger import logging
 
-print("Data inserted successfully!")
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def cv_to_json(self,file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def insert_data_mongodb(self,records,collection,database):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URI, tlsCAFile=ca)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+if __name__ == "__main__":
+    FILE_PATH = r"C:\Users\ASUS\Desktop\project\phishing.csv"
+    DATABASE = "Networksecurity"
+    COLLECTION = "phishing"
+
+    networkobj = NetworkDataExtract()
+    records = networkobj.cv_to_json(file_path=FILE_PATH)   # fixed method name
+    no_of_records = networkobj.insert_data_mongodb(
+        records=records,
+        collection=COLLECTION,
+        database=DATABASE
+    )
+    print(f"Number of records inserted: {no_of_records}")
