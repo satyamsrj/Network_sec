60dbbe865fe9af1bfdf961e03043b2cf78ff30ec
satyamsrj <sarojsatyam150@gmail.com>
2026-07-06 21:02:14 +0530

    ML utils Estimator are completely established

---
+++ Networksecurity/utils/ml_utils/model/estimator.py
@@ -0,0 +1,17 @@
+from Networksecurity.constant.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
+import os,sys
+
+from Networksecurity.exception.exception import NetworkSecurityException
+from Networksecurity.logging.logger import logging
+
+
+class NetworkMOdel:
+    def __init__(self,x):
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+        
